96cf85b89da96a15cfa0bf369b89ce107ab22a71
gabrielbrunop <gabrielbrunoes@gmail.com>
2024-04-25 00:44:02 -0300

    remove stylua mason default installation

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -402,9 +402,7 @@ require('lazy').setup({
       require('mason').setup()
 
       local ensure_installed = vim.tbl_keys(servers or {})
-      vim.list_extend(ensure_installed, {
-        'stylua',
-      })
+
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
       require('mason-lspconfig').setup {
