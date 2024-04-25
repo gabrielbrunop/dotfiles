fe71135e668f48781f0760934ada59adac0130bf
gabrielbrunop <gabrielbrunoes@gmail.com>
2024-04-25 00:45:16 -0300

    configure conform to support a variety of web languages

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -436,21 +436,37 @@ require('lazy').setup({
         desc = '[F]ormat buffer',
       },
     },
-    opts = {
-      notify_on_error = false,
-      format_on_save = function(bufnr)
-        local disable_filetypes = { c = true, cpp = true }
-        return {
-          timeout_ms = 500,
-          lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
-        }
-      end,
-      formatters_by_ft = {
-        lua = { 'stylua' },
-        javascript = { { 'prettierd', 'prettier' } },
-        typescript = { { 'prettierd', 'prettier' } },
-      },
-    },
+    config = function()
+      require("conform").setup({
+        notify_on_error = true,
+        format_on_save = function(bufnr)
+          local disable_filetypes = { c = true, cpp = true }
+          return {
+            timeout_ms = 500,
+            lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+          }
+        end,
+        formatters_by_ft = {
+          lua = { 'stylua' },
+          javascript = { { "prettierd", "prettier" } },
+          typescript = { { "prettierd", "prettier" } },
+          javascriptreact = { { "prettierd", "prettier" } },
+          typescriptreact = { { "prettierd", "prettier" } },
+          vue = { { "prettierd", "prettier" } },
+          css = { { "prettierd", "prettier" } },
+          scss = { { "prettierd", "prettier" } },
+          less = { { "prettierd", "prettier" } },
+          html = { { "prettierd", "prettier" } },
+          json = { { "prettierd", "prettier" } },
+          jsonc = { { "prettierd", "prettier" } },
+          yaml = { { "prettierd", "prettier" } },
+          markdown = { { "prettierd", "prettier" } },
+          ["markdown.mdx"] = { { "prettierd", "prettier" } },
+          graphql = { { "prettierd", "prettier" } },
+          handlebars = { { "prettierd", "prettier" } },
+        },
+      })
+    end,
   },
 
   {
